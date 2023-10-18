5c150516f4a46df9dfba3c08a3fa800a9313288c
asiagovincent11@gmail.com <asiagovincent11@gmail.com>
2023-10-18 21:32:20 +0300

    done with the lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  // Test the functions
+  console.log(employee);
+  console.log(updateEmployeeWithKeyAndValue(employee, "name", "Alice"));
+  console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "456 Elm St"));
+  console.log(deleteFromEmployeeByKey(employee, "name"));
+  console.log(destructivelyDeleteFromEmployeeByKey(employee, "streetAddress"));
+  
